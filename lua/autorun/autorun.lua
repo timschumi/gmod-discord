d4e2375a59ecd003ae9d2e3129f5ddf35cf92462
Tim Schumacher <timschumi@gmx.de>
2019-11-09 17:38:44 +0100

    Rename client/server to discord_cl/sv

--- lua/autorun/autorun.lua
+++ lua/autorun/autorun.lua
@@ -1,15 +1,15 @@
 AddCSLuaFile()
 AddCSLuaFile("printchat_cl.lua")
-AddCSLuaFile("client.lua")
+AddCSLuaFile("discord_cl.lua")
 
 resource.AddFile("materials/mute-icon.png")
 
 if CLIENT then
 	include("printchat_cl.lua")
-	include("client.lua")
+	include("discord_cl.lua")
 end
 
 if SERVER then
 	include("printchat_sv.lua")
-	include("server.lua")
+	include("discord_sv.lua")
 end
