9efb81adc7cbf1abe070bdf10b2f5c824f37d572
Tim Schumacher <timschumi@gmx.de>
2022-01-02 23:24:51 +0100

    Ensure that CHTTP finished loading before trying to use it

--- lua/discord_sv.lua
+++ lua/discord_sv.lua
@@ -34,7 +34,7 @@ function plymeta:getDiscordID() return ids:get(self:SteamID()) end
 function plymeta:setDiscordID(id) return ids:set(self:SteamID(), id) end
 
 
-if pcall(require, "chttp") then
+if pcall(require, "chttp") and CHTTP ~= nil then
 	log("Using CHTTP implementation.")
 	discordHTTP = CHTTP
 else
